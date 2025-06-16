a8a13eead712b8d899282034a70046870150c32d
yjw3165 <yjw3165@gmail.com>
2025-06-16 11:42:56 +0900

    점프 기능 추가

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <SDL.h>
 
-
 //SDL 초기화 , 윈도우 생성 , 렌더러 생성
 bool(InitSDL(SDL_Window** window, SDL_Renderer** renderer))
 {
@@ -53,7 +52,7 @@ SDL_Texture* LoadTexture(const char* path, SDL_Renderer* Renderer)
 	return TempTexture;
 }
 
-void InitTextureFrames(SDL_Rect Frames[], int count, int frameWidth, int frameHeght, int Columns)
+void InitTextureFrames(SDL_Rect Frames[], int count, int frameWidth, int frameHeght, int Columns, int Rows)
 {
 
 	for (int i = 0; i < count; i++)
@@ -86,12 +85,17 @@ int main()
 	}
 	
 	//텍스처로딩
-	SDL_Texture* idleL = LoadTexture("assets/idle_Left.bmp",Renderer);
-	SDL_Texture* idleR = LoadTexture("assets/idle_Right.bmp", Renderer);
+	//Idle
+	SDL_Texture* idleL = LoadTexture("assets/idle_left.bmp",Renderer);
+	SDL_Texture* idleR = LoadTexture("assets/idle_right.bmp", Renderer);
+	//Run
 	SDL_Texture* runL = LoadTexture("assets/run_left.bmp", Renderer);
 	SDL_Texture* runR = LoadTexture("assets/run_right.bmp", Renderer);
+	//Jump
+	SDL_Texture* JumpL = LoadTexture("assets/jump_left.bmp", Renderer);
+	SDL_Texture* JumpR = LoadTexture("assets/jump_right.bmp", Renderer);
 
-	SDL_Texture* Array_Textures[] = { idleL,idleR,runL,runR };
+	SDL_Texture* Array_Textures[] = { idleL,idleR,runL,runR,JumpL,JumpR };
 	
 	for (SDL_Texture* Texture : Array_Textures)
 	{
@@ -110,13 +114,16 @@ int main()
 	const int Frame_HEIGHT = 32;
 	const int IDLE_FRAME_COUNT = 9;
 	const int RUN_FRAME_COUNT = 8;
-	const int FRAME_COLUMNS = 3;
+	const int JUMP_FRAME_COUNT = 12;
+
 
 	//텍스처 프레임 초기화
 	SDL_Rect idleFrames[9];
-	InitTextureFrames(idleFrames, IDLE_FRAME_COUNT,Frame_WIDTH,Frame_HEIGHT,FRAME_COLUMNS);
+	InitTextureFrames(idleFrames, IDLE_FRAME_COUNT,Frame_WIDTH,Frame_HEIGHT,3,3);
 	SDL_Rect RunFrames[8];
-	InitTextureFrames(RunFrames, RUN_FRAME_COUNT, Frame_WIDTH, Frame_HEIGHT, FRAME_COLUMNS);
+	InitTextureFrames(RunFrames, RUN_FRAME_COUNT, Frame_WIDTH, Frame_HEIGHT,3,3);
+	SDL_Rect JumpFrames[12];
+	InitTextureFrames(JumpFrames, JUMP_FRAME_COUNT, Frame_WIDTH, Frame_HEIGHT, 3,4);
 	
 	//프레임 관련 변수 초기화
 	int FrameIndex = 0;
@@ -124,6 +131,11 @@ int main()
 	Uint32 frameInterval = 0;
 	
 	//상태 초기화
+	float velocityY = 0.0f;
+	bool isJumping = false;
+	const int groundY = 224;
+	float gravity = 0.5f;
+
 	bool wasLeft = false;
 	SDL_Texture* CurrentTexture = idleR;
 	SDL_Rect* CurrentFrame = idleFrames;
@@ -147,6 +159,8 @@ int main()
 		Uint32 Now = SDL_GetTicks();
 
 		//Key에 따른 세팅 적용
+
+		//이동
 		if (keyStates[SDL_SCANCODE_A])
 		{
 			dstRect.x -= 4;
@@ -160,14 +174,77 @@ int main()
 			isMoving = true;
 		}
 
+		//점프
+		if (keyStates[SDL_SCANCODE_SPACE] && !isJumping)
+		{
+			std::cout << "Press SpaceBar" << std::endl;
+			velocityY = -10.0f;
+			FrameIndex = 0;
+			isJumping = true;
+		}
+
+		//esc 누르면 종료
+		else if (keyStates[SDL_SCANCODE_ESCAPE])
+		{
+			running = false;
+		}
+
+		//Jump
+		if (isJumping)
+		{
+			//y좌표에 velocity를 더해서 적용하고
+			dstRect.y += static_cast<int>(velocityY);
+			//velocity에 gravity를 더해서 중력을 표현
+			velocityY += gravity;
+
+			//y좌표가 groundY와 같아지거나 커지면(더 아래로 내려가면) 초기화.
+			if (dstRect.y >= groundY)
+			{
+				dstRect.y = groundY;
+				velocityY = 0.0f;
+				isJumping = false;
+
+				std::cout << "Landed" << std::endl;
+
+			}
+		}
+
 		//Run ? Idle ?
-		if (isMoving)
+
+		if (isJumping)
+		{
+			isMoving = false;
+			CurrentFrame = JumpFrames;
+			CurrentTexture = wasLeft ? JumpL : JumpR;
+			frameInterval = 150;
+
+			if (velocityY < 0)
+			{
+				FrameIndex = (Now - lastFrameTime >= frameInterval) ? (FrameIndex + 1) % JUMP_FRAME_COUNT : FrameIndex;
+			}
+			else if (velocityY == 0)
+			{
+				FrameIndex = 6;
+			}
+			else if (velocityY > 0)
+			{
+				FrameIndex = (Now - lastFrameTime >= frameInterval) ? (6 + 1) % JUMP_FRAME_COUNT : FrameIndex;
+			}
+			else
+			{
+				FrameIndex = (Now - lastFrameTime >= frameInterval) ? (8 + 1) % JUMP_FRAME_COUNT : FrameIndex;
+			}
+
+		}
+
+		else if (isMoving)
 		{
 			CurrentFrame = RunFrames;
 			CurrentTexture = wasLeft ? runL : runR;
 			frameInterval = 25;
 			FrameIndex = (Now - lastFrameTime >= frameInterval) ? (FrameIndex + 1) % RUN_FRAME_COUNT : FrameIndex;
 		}
+		
 		else
 		{
 			CurrentFrame = idleFrames;
@@ -175,6 +252,9 @@ int main()
 			frameInterval = 200;
 			FrameIndex = (Now - lastFrameTime >= frameInterval) ? (FrameIndex + 1) % IDLE_FRAME_COUNT : FrameIndex;
 		}
+		
+		
+		
 
 		if (Now - lastFrameTime >= frameInterval)
 		{
